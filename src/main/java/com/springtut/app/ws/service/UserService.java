4065d8bbb88fe8c5ccc00ac932834f9468be1cb5
manaraph <manaraph225@gmail.com>
2019-12-18 01:05:24 +0100

    Add spring security, encrypt password and open up the signup endpoint

--- src/main/java/com/springtut/app/ws/service/UserService.java
+++ src/main/java/com/springtut/app/ws/service/UserService.java
@@ -1,7 +1,9 @@
 package com.springtut.app.ws.service;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 import com.springtut.app.ws.shared.dto.UserDto;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
 	UserDto createUser(UserDto user);
 }
--- src/main/java/com/springtut/app/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/springtut/app/ws/service/impl/UserServiceImpl.java
@@ -2,6 +2,8 @@ package com.springtut.app.ws.service.impl;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -43,6 +45,12 @@ public class UserServiceImpl implements UserService {
 		BeanUtils.copyProperties(storedUserDetails, returnValue);
 		
 		return returnValue;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
