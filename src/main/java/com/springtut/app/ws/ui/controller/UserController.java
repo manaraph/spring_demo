4b8f3bcd3d0c692586e4110e942f6be8af36761c
manaraph <manaraph225@gmail.com>
2019-12-28 13:10:17 +0100

    Allow JSON/XML support for response

--- src/main/java/com/springtut/app/ws/ui/controller/UserController.java
+++ src/main/java/com/springtut/app/ws/ui/controller/UserController.java
@@ -2,6 +2,7 @@ package com.springtut.app.ws.ui.controller;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,7 +24,10 @@ public class UserController {
 	@Autowired
 	UserService userService;
 
-	@GetMapping(path="/{id}")
+	@GetMapping(
+		path="/{id}", 
+		produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE}
+	)
 	public UserRest getUser(@PathVariable String id) {
 		UserRest returnValue = new UserRest();
 		UserDto userDto = userService.getUserByUserId(id);
@@ -31,7 +35,10 @@ public class UserController {
 		return returnValue;
 	}
 	
-	@PostMapping
+	@PostMapping(
+			consumes = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE},
+			produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE}
+		)
 	public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) {
 		UserRest returnValue = new UserRest();
 		
